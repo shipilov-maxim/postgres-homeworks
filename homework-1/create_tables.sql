08d98247438d44f1ce2b22ba20e3ac732de0a1b8
shipilov_maxim <skam3840@gmail.com>
2023-11-18 23:57:36 +0400

    HW 2

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,26 +1,26 @@
 -- SQL-команды для создания таблиц
 CREATE TABLE customers
 (
-    customer_id varchar PRIMARY KEY,
-    company_name varchar NOT NULL,
-    contact_name varchar NOT NULL
+    customer_id varchar(5) PRIMARY KEY,
+    company_name varchar(50) NOT NULL,
+    contact_name varchar(50) NOT NULL
 );
 
 CREATE TABLE employees
 (
     employee_id int PRIMARY KEY,
-    first_name varchar NOT NULL,
-	last_name varchar NOT NULL,
-	title varchar NOT NULL,
-    birth_date varchar NOT NULL,
+    first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(50) NOT NULL,
+    birth_date date NOT NULL,
 	notes text NOT NULL
 );
 
 CREATE TABLE orders
 (
 	order_id int PRIMARY KEY,
-    customer_id varchar REFERENCES customers(customer_id),
+    customer_id varchar(5) REFERENCES customers(customer_id),
     employee_id int REFERENCES employees(employee_id),
-	order_date varchar NOT NULL,
-	ship_city varchar NOT NULL
+	order_date date NOT NULL,
+	ship_city varchar(20) NOT NULL
 );
--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -1,14 +1,15 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. заказы, доставленные в страны France, Germany, Spain (таблица orders, колонка ship_country)
-SELECT ...
+SELECT * FROM orders WHERE ship_country in ('France', 'Germany', 'Spain');
 
 -- 2. уникальные страны и города, куда отправлялись заказы, отсортировать по странам и городам (таблица orders, колонки ship_country, ship_city)
-
+SELECT DISTINCT SHIP_COUNTRY, SHIP_CITY FROM ORDERS ORDER BY SHIP_COUNTRY, SHIP_CITY
 
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
-
+SELECT AVG(SHIPPED_DATE-ORDER_DATE) FROM ORDERS WHERE SHIP_COUNTRY IN ('Germany')
 
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
-
+SELECT MIN(unit_price), MAX(unit_price) FROM PRODUCTS WHERE discontinued NOT IN (1)
 
 -- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20 (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
+SELECT MIN(unit_price), MAX(unit_price) FROM PRODUCTS WHERE discontinued NOT IN (1) AND units_in_stock>20
